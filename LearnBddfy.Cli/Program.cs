d5bd6e6f6e3f3d7e246ab5d20524d1fe7ec35158
Shaw Innes <shaw@immortal.net.au>
2019-12-16 21:16:54 +1000

    Initial Commit

---
+++ LearnBddfy.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnBddfy.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LearnBddfy.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LearnBddfy.Core
+{
+    public class Class1
+    {
+    }
+}
